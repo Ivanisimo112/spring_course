1358f3e07178edcfc07c5c54ff86c9b0d6e23daa
Ivanisimo112 <vane4ka3334@gmail.com>
2021-08-09 15:06:46 +0300

    +new class Test4

---
+++ src/spring_introduction/Person.java
@@ -0,0 +1,36 @@
+package spring_introduction;
+
+public class Person {
+    private Pet pet;
+    private String surname;
+    private int age;
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public void callYourPet(){
+        System.out.println("Hello,my pet!");
+        pet.say();
+    }
+}
